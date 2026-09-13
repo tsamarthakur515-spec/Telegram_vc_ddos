0e4409a702954a6a53c82c02e2b5c1ebeb6314e5
tsamarthakur515-spec <tsamarthakur515@gmail.com>
2026-09-13 13:33:42 +0545

    Update config.py

--- config.py
+++ config.py
@@ -48,7 +48,7 @@ class Config:
             api_hash=str(required["e4a5fa251e2e055f26e5c2add8401530"]),
             bot_token=str(required["8706433441:AAFJxJ7j3Lf9HANX_-V2cM0ygMkylZIT8ic"]),
             session_string=str(required["BQC86fAAw2rC7DEINQ1pozAAMKIhGdfL15ppowCMJ9VZMl3VEBELTU4sFh5VaL_44cobX4WnmJOtcycRALtY5eotO0_QCA0lSapR1hTv97rysTtE_wlSDCcqL-A53YszOG8LXmfrDSwde9Lqny7FDb6HM-FAx2slKyg8FiuwzdSoIAPabcSZOfMJdQRiOeVRlhuALTYtf_9R5zdQhKl1R_95G7D-dYZ_hGTEiNqa-XBMaMFU8MJqKtuqrSZ4YtUG8YXSRnS44bSSZsohir8Nz3lx25ir8TzVsZDnwzXvtYxXATNSiVkQ1D7yzgSwb13EoeQ3L3F_UbfSmY6kunc7leKyWr8dbQAAAAH-nwzIAA"]),
-            admin_id=int(os.getenv("8841848847")) if os.getenv("ADMIN_ID") else None,
+            admin_id=int(os.getenv("8841848847")) if os.getenv("8841848847") else None,
             max_duration=max_duration,
             max_threads=max_threads,
             scan_limit=scan_limit,
